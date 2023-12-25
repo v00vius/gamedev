26f8faa827343ecb3b56caefdba906c148383882
Vladimir Gavrilyuk <voovius@yahoo.com>
2023-12-25 18:20:02 +0300

    update

--- java/step001/src/Main.java
+++ java/step001/src/Main.java
@@ -16,6 +16,10 @@ public class Main {
 
         Entity[] shapes = loader.load();
 
+        Vectortest();
+    }
+
+    private static void Vectortest() {
         Vector2 v1 = new Vector2(100, 100);
         Vector2 v2 = new Vector2(1, 1);
         Vector2 v3 = new Vector2();
@@ -27,10 +31,36 @@ public class Main {
         v3.add(v2).sub(v2).mul(0.5f);
         System.out.println("((v3 += v2) -= v2) * 0.5: " + v3);
         System.out.println("v3.length = " + v3.length());
+        System.out.println("v3 angle: " + v3.angle());
+        System.out.println("v3 angle: " + v3.angle());
 
-        for(float a = 0; a <= 360; a += 10) {
+        v3.rotateRelative(-v3.angle());
+        System.out.println("v3 new angle: " + v3.angle() + ", v3=" + v3);
+        v3.rotateRelative(90f);
+        System.out.println("v3 rotation (+90 deg), angle: " + v3.angle() + ", v3=" + v3);
+
+        for(float a = 0; a <= 360; a += 30) {
             v1.projection(100, a);
-            System.out.println("projection of 100, angle " + a + ": " + v1);
+            System.out.println("projection of 100, bearing " + a + ": " + v1);
         }
+
+//        Vector2 a1 = new Vector2(10, 10);
+//        Vector2 b1 = new Vector2(50, 20);
+//        Vector2 a2 = new Vector2(10, 50);
+//        Vector2 b2 = new Vector2(40, 00);
+
+//        Vector2 a1 = new Vector2(10, 10);
+//        Vector2 b1 = new Vector2(50, 10);
+//        Vector2 a2 = new Vector2(10, 50);
+//        Vector2 b2 = new Vector2(40, 50);
+
+        Vector2 a1 = new Vector2(10, 10);
+        Vector2 b1 = new Vector2(50, 10);
+        Vector2 a2 = new Vector2(9.99999f, 00);
+        Vector2 b2 = new Vector2(9.99999f, 50);
+
+        boolean ok = Vector2.isIntersect(a1, b1, a2, b2);
+
+        System.out.println("Intersection: " + ok);
     }
 }
--- java/step001/src/types/Vector2.java
+++ java/step001/src/types/Vector2.java
@@ -41,19 +41,34 @@ public class Vector2 {
 
         return this;
     }
+    public Vector2 abs() {
+        x = Math.abs(x);
+        y = Math.abs(y);
+
+        return this;
+    }
     public Vector2 mul(float m) {
         x *= m;
         y *= m;
 
         return this;
     }
-    public Vector2 projection(float length, float bearing) {
-        float rad = (float) Math.toRadians(-bearing + 90.0);
+    public Vector2 projection(float length, float angle) {
+        float rad = (float) Math.toRadians(angle);
 
         x = length * (float) Math.cos((double) rad);
         y = length * (float) Math.sin((double) rad);
 
         return this;
+    }
+    public float angle() {
+        if(Math.abs(x) < 1e-6)
+            return 90f;
+
+        return (float) Math.toDegrees(Math.atan2(y, x));
+    }
+    public Vector2 rotateRelative(float delta) {
+        return projection(length(), delta + angle());
     }
     public float distance(Vector2 b) {
         float dx = x - b.x;
@@ -63,6 +78,54 @@ public class Vector2 {
     }
     public float length() {
         return (float) Math.sqrt((double)(x * x) + (double)(y * y));
+    }
+
+    public static boolean isIntersect(Vector2 a1, Vector2 b1,
+                                        Vector2 a2, Vector2 b2) {
+
+        System.out.println("[0]");
+        System.out.println("a1=" + a1);
+        System.out.println("b1=" + b1);
+        System.out.println("a2=" + a2);
+        System.out.println("b2=" + b2);
+//  1
+        b1.sub(a1);
+        a2.sub(a1);
+        b2.sub(a1);
+        a1.sub(a1);
+
+        System.out.println("[1]");
+        System.out.println("a1=" + a1);
+        System.out.println("b1=" + b1);
+        System.out.println("a2=" + a2);
+        System.out.println("b2=" + b2);
+//  2
+        float angle = b1.angle();
+
+        b1.rotateRelative(-angle);
+        a2.rotateRelative(-angle);
+        b2.rotateRelative(-angle);
+
+        System.out.println("[2]");
+//        System.out.println("a1=" + a1);
+        System.out.println("b1=" + b1);
+        System.out.println("a2=" + a2);
+        System.out.println("b2=" + b2);
+//  3
+        if(Math.abs(a2.y - b2.y) < 1e-5)
+            return false;
+
+        Vector2 c = new Vector2(b2);
+
+        c.sub(a2).abs();
+
+        float x = c.x < 1e-5 ? a2.x : a2.y * c.x / c.y;
+
+        System.out.println("[3]");
+        System.out.println("c=" + c);
+        System.out.println("x=" + x);
+
+        return (-1e-5f <= x && x <= (b1.x + 1e-5f));
     }
 
     @Override
