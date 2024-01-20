ae204469d0638ec4ab4e50e92ec8ee588a6139da
Vladimir Gavrilyuk <voovius@yahoo.com>
2024-01-20 22:46:15 +0300

    update

---
+++ java/maze/src/Main.java
@@ -0,0 +1,134 @@
+
+
+import imgui.*;
+import imgui.app.Application;
+import imgui.app.Configuration;
+import imgui.flag.*;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+
+public class Main extends Application {
+private UserInput userInput;
+private long msDuration;
+private boolean done = false;
+private long frameCount = 0;
+private float frameTime = 0.f;
+
+public Main()
+{
+        super();
+}
+
+public static void main(final String[] args)
+{
+        launch(new Main());
+        System.exit(0);
+}
+@Override
+protected void configure(final Configuration config)
+{
+        config.setTitle("ImGUI Version " + ImGui.getVersion() + ", https://github.com/SpaiR/imgui-java");
+        config.setWidth(1920);
+        config.setHeight(1080);
+}
+@Override
+protected void preRun()
+{
+        userInput = new UserInput();
+}
+@Override
+protected void postRun() {
+}
+
+@Override
+protected void preProcess()
+{
+        userInput.frame();
+        msDuration = System.currentTimeMillis();
+}
+@Override
+public void process()
+{
+        mainMenu();
+}
+@Override
+protected void postProcess()
+{
+        msDuration = System.currentTimeMillis() - msDuration;
+
+        int fps = msDuration == 0 ? 999 : (int)(0.5f + 1000.f / (float) msDuration);
+
+        ImGui.text(String.format("Scene latency: %d ms (%3d FPS)", msDuration, fps));
+}
+private void mainMenu()
+{
+        if (++frameCount % 100 == 0)
+                frameTime = ImGui.getIO().getDeltaTime();
+
+        ImGui.text(String.format("Frame time: %5.1f ms", frameTime * 1000.f));
+        ImGui.text("FPS: " + (int) (0.5f + 1.f / frameTime));
+        ImGui.text(String.format("Time: %5.1f ", ImGui.getTime()));
+
+        ImGui.colorEdit3("Background Color", this.getColorBg().data);
+
+        if (ImGui.beginMainMenuBar()) {
+                if (ImGui.beginMenu("File")) {
+                        ImGui.menuItem("[File menu]", null, false, false);
+
+                        if (ImGui.menuItem("Quit", "Alt+F4")) {
+                                done = true;
+                        }
+
+                        ImGui.endMenu();
+                }
+
+                if (ImGui.beginMenu("Settings")) {
+                        if (ImGui.menuItem("Stats", "S")) {
+                        }
+
+                        ImGui.endMenu();
+                }
+
+                if (ImGui.beginMenu("Help")) {
+                        if (ImGui.menuItem("Controls", "C")) {
+                        }
+
+                        ImGui.endMenu();
+                }
+
+                ImGui.endMainMenuBar();
+        }
+}
+@Override
+protected void initImGui(final Configuration config)
+{
+        super.initImGui(config);
+
+        final ImGuiIO io = ImGui.getIO();
+
+        io.setIniFilename(null);                                // We don't want to save .ini file
+        io.addConfigFlags(ImGuiConfigFlags.NavEnableKeyboard);  // Enable Keyboard Controls
+        io.addConfigFlags(ImGuiConfigFlags.DockingEnable);      // Enable Docking
+        io.addConfigFlags(ImGuiConfigFlags.ViewportsEnable);    // Enable Multi-Viewport / Platform Windows
+        io.setConfigViewportsNoTaskBarIcon(true);
+
+        initFonts(io);
+}
+
+private void initFonts(final ImGuiIO io)
+{
+        io.getFonts().addFontDefault(); // Add default font for latin glyphs
+}
+private static byte[] loadFromResources(String name)
+{
+        try {
+                return Files.readAllBytes(Paths.get(Main.class.getResource(name).toURI()));
+        } catch (IOException | URISyntaxException e) {
+                throw new RuntimeException(e);
+        }
+}
+}
---
+++ java/maze/src/Vector2.java
@@ -0,0 +1,217 @@
+package types;
+
+import imgui.ImVec2;
+
+public class Vector2 {
+        public float x;
+        public float y;
+
+        public Vector2()
+        {
+                x = y = 0.f;
+        }
+
+        public Vector2(Vector2 v)
+        {
+                this.set(v);
+        }
+
+        public Vector2(ImVec2 v)
+        {
+                this.set(v);
+        }
+
+        public Vector2(float x, float y)
+        {
+                set(x, y);
+        }
+
+        public Vector2 set(float x, float y)
+        {
+                this.x = x;
+                this.y = y;
+
+                return this;
+        }
+
+        public Vector2 set(Vector2 v)
+        {
+                this.x = v.x;
+                this.y = v.y;
+
+                return this;
+        }
+
+        public Vector2 set(ImVec2 v)
+        {
+                this.x = v.x;
+                this.y = v.y;
+
+                return this;
+        }
+
+
+        public Vector2 add(Vector2 b)
+        {
+                x += b.x;
+                y += b.y;
+
+                return this;
+        }
+
+        public Vector2 add(float dx, float dy)
+        {
+                x += dx;
+                y += dy;
+
+                return this;
+        }
+
+        public Vector2 sub(Vector2 b)
+        {
+                x -= b.x;
+                y -= b.y;
+
+                return this;
+        }
+
+        public Vector2 sub(float dx, float dy)
+        {
+                x -= dx;
+                y -= dy;
+
+                return this;
+        }
+
+        public Vector2 neg()
+        {
+                x = -x;
+                y = -y;
+
+                return this;
+        }
+
+        public Vector2 abs()
+        {
+                x = Math.abs(x);
+                y = Math.abs(y);
+
+                return this;
+        }
+
+        public Vector2 mul(float m)
+        {
+                x *= m;
+                y *= m;
+
+                return this;
+        }
+
+        public boolean lessOrEqual(Vector2 v)
+        {
+                return x <= v.x && y <= v.y;
+        }
+
+        public Vector2 projection(float length, float angleRadians)
+        {
+                x = length * (float) Math.cos((double) angleRadians);
+                y = length * (float) Math.sin((double) angleRadians);
+
+                return this;
+        }
+
+        public float angle()
+        {
+                float len = length();
+
+                return (float) Math.acos((double) (this.x / len));
+        }
+
+        public Vector2 rotate(float radians)
+        {
+                float ca = (float) Math.cos(radians);
+                float sa = (float) Math.sin(radians);
+
+                float tx = ca * x - sa * y;
+                float ty = sa * x + ca * y;
+
+                x = tx;
+                y = ty;
+
+                return this;
+        }
+
+        public float distance(Vector2 b)
+        {
+                float dx = x - b.x;
+                float dy = y - b.y;
+
+                return (float) Math.sqrt((double) (dx * dx) + (double) (dy * dy));
+        }
+
+        public float length()
+        {
+                return (float) Math.sqrt((double) (x * x) + (double) (y * y));
+        }
+
+        public boolean isCollided(float r, Vector2 center1, float r2)
+        {
+                Vector2 v = new Vector2(this);
+
+                v.sub(center1);
+
+                return (v.x * v.x + v.y * v.y) < (r + r2) * (r + r2);
+        }
+
+        public static Vector2 intersect(Vector2 a1, Vector2 b1,
+                                        Vector2 a2, Vector2 b2)
+        {
+                Vector2 v1 = new Vector2(b1).sub(a1);
+                Vector2 v2 = new Vector2(b2).sub(a2);
+//                Precision eps = Precision.create(1e-5f);
+
+//      Vectors are parallel
+                if ((Math.abs(v1.x) < 1e-5 && Math.abs(v2.x) < 1e-5) ||
+                        (Math.abs(v1.y) < 1e-5 && Math.abs(v2.y) < 1e-5))
+                        return null;
+
+                if (Math.abs(v1.x) < 1e-5) {
+                        float yc = a2.y + (a1.x - a2.x) * v2.y / v2.x;
+
+                        if (yc < Math.min(a1.y, b1.y) - 1e-5 ||
+                                yc > Math.max(a1.y, b1.y) + 1e-5)
+                                return null;
+
+                        return new Vector2(a1.x, yc);
+                }
+
+                if (Math.abs(v2.x) < 1e-5) {
+                        float yc = a1.y + Math.abs(a2.x - a1.x) * v1.y / v1.x;
+
+                        if (yc < Math.min(a2.y, b2.y) - 1e-5 ||
+                                yc > Math.max(a2.y, b2.y) + 1e-5)
+                                return null;
+
+                        return new Vector2(a1.x, yc);
+                }
+
+                float c1 = v1.y / v1.x;
+                float c2 = v2.y / v2.x;
+
+                float d = c1 * a1.x - a1.y - c2 * a2.x + a2.y;
+                float xc = d / (c1 - c2);
+                float yc = c1 * (xc - a1.x) + a1.y;
+
+                if (xc < Math.min(a1.x, b1.x) - 1e-5 ||
+                        xc > Math.max(a1.x, b1.x) + 1e-5)
+                        return null;
+
+                return new Vector2(xc, yc);
+        }
+
+        @Override
+        public String toString()
+        {
+                return "{" + x + ", " + y + "}";
+        }
+}
