86c08264b28c539616cad37b08746216a8de5379
Vladimir Gavrilyuk <voovius@yahoo.com>
2024-01-23 22:07:58 +0300

    update

--- java/mesh3/src/MeshBuilder.java
+++ java/mesh3/src/MeshBuilder.java
@@ -1,21 +1,24 @@
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class MeshBuilder {
 private Precision precision;
-private Set<Point> positions;
+private Map<Point, Point> positions;
 private List<Integer> indices;
 
 public MeshBuilder(float epsilon) {
     this.precision = Precision.create(epsilon);
-    positions = new HashSet<>();
+    positions = new TreeMap<>();
     indices = new ArrayList<>();
 }
 
-private class Point implements Comparable <Point> {
+private Point get(Point p)
+{
+        Point pos = positions.putIfAbsent(p, p);
+
+        return pos == null ? p : pos;
+}
+public class Point implements Comparable <Point> {
 public float x;
 public float y;
 public float z;
@@ -26,15 +29,38 @@ public Point(float x, float y, float z) {
     this.z = z;
 }
 
+public Point(float x, float y)
+{
+    this.x = x;
+    this.y = y;
+    this.z = 0.f;
+}
+
+@Override
+public boolean equals(Object o)
+{
+    if (this == o) {
+        return true;
+    }
+
+    if (o == null || getClass() != o.getClass()) {
+        return false;
+    }
+
+    Point point = (Point) o;
+
+    return 0 == this.compareTo(point);
+}
+
 @Override
 public int compareTo(MeshBuilder.Point p) {
-    int cmp = precision.floatCompare(x, p.x);
+    int cmp = precision.compare(x, p.x);
 
     if(cmp == 0)
-        cmp = precision.floatCompare(y, p.y);
+        cmp = precision.compare(y, p.y);
 
     if(cmp == 0)
-        cmp = precision.floatCompare(z, p.z);
+        cmp = precision.compare(z, p.z);
 
     return cmp;
 }
--- java/mesh3/src/Precision.java
+++ java/mesh3/src/Precision.java
@@ -7,34 +7,18 @@ static public Precision create(float eps)
 {
         return new Precision(eps);
 }
-
 private Precision(float epsilon)
 {
         this.epsilon = epsilon;
 }
-public int floatCompare(float f1, float f2)
+
+@Override
+public int compare(Float f1, Float f2)
 {
         if (Math.abs(f1 - f2) <= epsilon)
                 return 0;
 
         return f1 < f2 ? -1 : 1;
-}
-public boolean equals(float f1, float f2)
-{
-        return 0 == compare(f1, f2);
-}
-public boolean lessThan(float f1, float f2)
-{
-        return -1 == compare(f1, f2);
-}
-public boolean greaterThan(float f1, float f2)
-{
-        return 1 == compare(f1, f2);
-}
-
-@Override
-public int compare(Float o1, Float o2) {
-        return floatCompare(o1, o2);
 }
 
 }
